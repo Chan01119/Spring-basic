0698e6333a80745a3a753892d4eba2e9ea9b20d5
Chan01119 <cykimjejus112@gmail.com>
2025-03-11 14:53:31 +0900

    feat : 심플 url 매핑 예시 (완)

--- src/main/java/org/codenova/spring/controller/IndexHandler.java
+++ src/main/java/org/codenova/spring/controller/IndexHandler.java
@@ -3,10 +3,12 @@ package org.codenova.spring.controller;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.HttpRequestHandler;
 
 import java.io.IOException;
 
+@Component("help")
 public class IndexHandler implements HttpRequestHandler {
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
