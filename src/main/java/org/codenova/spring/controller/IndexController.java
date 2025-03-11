4c588fb5868ae2d325aae8a1fd6912b10197751f
Chan01119 <cykimjejus112@gmail.com>
2025-03-11 16:34:48 +0900

    feat : beanNameView 예시

--- src/main/java/org/codenova/spring/controller/IndexController.java
+++ src/main/java/org/codenova/spring/controller/IndexController.java
@@ -3,6 +3,7 @@ package org.codenova.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /*
     RequestMappingHandlerMapping 을 쓰게 되면 HttpRequestHandler 라는 interface 를 토대로 만들 필요가 없다,
@@ -12,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping({"/", "/index"})
-    public void indexHandle() {
+    public ModelAndView indexHandle() {
         System.out.println("IndexHandler.index");
+
+        // ModelAndView mav = new ModelAndView();
+        // mav.setViewName("index");
+
+        ModelAndView mav = new ModelAndView("indexView");
+        mav.addObject("msg", "Hello Spring");   //req.setAttribute ..
+        mav.addObject("numbers", new int[]{1,2,3,4});
+
+        return mav;
     }
 
     @RequestMapping("/help")
