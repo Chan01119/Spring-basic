e3136e86266cb663fecf55405d86155486e6f5cc
Chan01119 <cykimjejus112@gmail.com>
2025-03-12 17:03:49 +0900

    feat : 핸들러메서드 인자설정방법 (진행중..)

--- src/main/java/org/codenova/spring/controller/StudyController.java
+++ src/main/java/org/codenova/spring/controller/StudyController.java
@@ -3,10 +3,16 @@ package org.codenova.spring.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.codenova.spring.model.TravelSchedule;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 /*
     @RequestMapping handler methods have a flexible signature and can choose from a range of supported controller method arguments and return values.
@@ -50,7 +56,78 @@ public class StudyController {
     @RequestMapping("/three")
     public void studyThreeHandle(@RequestParam("code") String code, @RequestParam("size") int size) {
         System.out.println("StudyController.studyThreeHandle()");
-        System.out.println("code =" +code);
-        System.out.println("size =" +size);
+        System.out.println("code =" + code);
+        System.out.println("size =" + size);
     }
+
+    /*
+        @RequestParam 을 설정하면 필수 파라미터로 처리된다. 만약 안넘어와도 되는 파라미터라면..? 추가 처리가 필요하다.
+     */
+    @RequestMapping("/four")
+    public void studyFourHandle(@RequestParam("titleId") int titleId,
+                                @RequestParam("sort") @Nullable String sort,
+                                @RequestParam("page") @Nullable Integer page) {
+        System.out.println("StudyController.studyFourHandle()");
+        System.out.println("sort =" + sort);
+        System.out.println("page =" + page);
+
+        sort = sort == null ? "DESC" : sort;
+        page = sort == null ? 1 : page;
+    }
+
+    @RequestMapping("/five")
+    public void studyFiveHandle(@RequestParam("titleId") int titleId,
+                                @RequestParam("sort") Optional<String> sort,
+                                @RequestParam("page") Optional<Integer> page) {
+        System.out.println("StudyController.studyFiveHandle()");
+
+
+        // Optional 객체에서 get() 으로 value를 확보하면 값을 안가지고 있으면 못뽑음(익셉션 발생)
+        // boolean isPresent() , boolean isEmpty()
+        // String sortValue = sort.get();
+        // String sortValue = sort.isPresent() ? sort.get() : "DESC";
+        Integer pageValue = page.orElse(1);
+
+        /*
+         String sortValue = sort.isPresent() ? sort.get() : "DESC";
+         이거를 orElse 쓰는 방식으로 코드 리팩토링을 진행해본다면 ?
+         */
+        String sortValue = sort.orElse("DESC");
+
+        System.out.println("sortValue =" + sortValue);
+        System.out.println("pageValue =" + pageValue);
+    }
+
+    /*
+        @ModeAttribute :
+        The @ModelAttribute method parameter annotation binds request parameters onto a model object.
+        HTTP 요청에서 전달된 데이터를 자바 객체(Model Object)에 자동으로 매핑해주는 역할을 합니다
+
+        ModeAttribute 로 받을때 할 수 있는 아주 효율적인 기능이 하나 더 있다.
+
+     */
+
+    @RequestMapping("/six")
+    public void studySixHandle(@ModelAttribute TravelSchedule schedule) {
+        System.out.println("StudyController.studySixHandle");
+
+        System.out.println(schedule.toString() );
+    }
+
+    /*
+       @RequestMapping methods can be mapped using URL patterns.
+            Captured URI variables can be accessed with @PathVariable.
+
+     */
+
+    @RequestMapping("/seven/{subject}")
+    public void studySevenHandle(@PathVariable String subject) {
+        System.out.println("StudyController.studySevenHandle()");
+        System.out.println("subject =" + subject);
+    }
+
+    /*
+      return 타입은 거의 String을 씀.
+        void 를 쓰면, mapping 잡힌 그 경로를 뷰이름으로 사용한다.
+     */
 }
